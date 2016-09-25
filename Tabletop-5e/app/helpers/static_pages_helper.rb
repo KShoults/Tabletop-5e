1bf53c788bcfe310ac7f5968473ab2b9b6826ae8
Kobi Shoults <kobishoults@gmail.com>
2016-09-25 04:19:20 +0000

    Update test suite.

--- Tabletop-5e/app/helpers/static_pages_helper.rb
+++ Tabletop-5e/app/helpers/static_pages_helper.rb
@@ -1,8 +1,18 @@
 module StaticPagesHelper
+  
   def read_chardata(name = '')
     if name != nil then
       char0 = Character.find_by(name: name)
       char0.shape
+    end
+  end
+  
+  def full_title(page_title = '')
+    base_title = 'Tabletop-5e'
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
     end
   end
 end
--- Tabletop-5e/test/controllers/static_pages_controller_test.rb
+++ Tabletop-5e/test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get root route" do
-    get :/
-    assert_response :success
-  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Tabletop5e"
+    assert_select "title", "Tabletop-5e"
   end
 
   test "should get table" do
     get :table
     assert_response :success
-    assert_select "title", "Tabletop | Tabletop5e"
+    assert_select "title", "Tabletop | Tabletop-5e"
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Tabletop-5e"
   end
 end
