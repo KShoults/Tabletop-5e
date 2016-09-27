43d65e0cf8788c6a21cb90a9adcda5bb1ad80d63
Kobi Shoults <kobishoults@gmail.com>
2016-09-27 04:28:54 +0000

    Add character deletion to finish character model.

--- Tabletop-5e/app/controllers/characters_controller.rb
+++ Tabletop-5e/app/controllers/characters_controller.rb
@@ -21,11 +21,18 @@ class CharactersController < ApplicationController
   def index
   end
   
+  def destroy
+    @character = Character.find(params[:id])
+    @character.delete
+    flash[:success] = "Character deleted successfully."
+    redirect_to characters_path
+  end
+  
   private
   
     def character_params
       params.require(:character).permit(:name, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma,
-                                   :character_class, :race, :backgroud, :alignment, :player_name, :experience_points,
+                                   :character_class, :race, :background, :alignment, :player_name, :experience_points,
                                    :speed, :max_hit_points, :type_hit_dice, :languages, :inventory)
     end
 end
